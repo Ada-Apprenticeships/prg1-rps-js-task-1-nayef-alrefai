f186b3d17fc848640c69c947b795988e6d74631f
Nayef.Alrefai <159164377+nayef-alrefai@users.noreply.github.com>
2024-10-16 11:58:43 +0000

    v1

--- rps.js
+++ rps.js
@@ -1,5 +1,21 @@
-function rockPaperScissors(player1, player2) {
+const readlineSync = require('readline-sync');
 
+function PlayerChoice() {
+  let choice = readlineSync.question('Please enter rock paper or scissors').toLowerCase();
+  while (choice !== 'rock' && choice !== 'paper' && choice !== 'scissors') {
+    choice = readlineSync.question ('Please input a valid choice of either rock, paper or scissors').toLowerCase();
+  }
+   return PlayerChoice; 
+
+}
+
+User1 = PlayerChoice()
+User2 = PlayerChoice()
+
+console.log(User1 + User2);
+
+/*function rockPaperScissors(player1, player2) {
+  
   
    
 }
@@ -17,3 +33,4 @@ function rockPaperScissors(player1, player2) {
 module.exports = {
   rockPaperScissors,
 }
+  */

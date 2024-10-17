0d5d08c3f573e5b66c0036e4334e81c801e8571a
Nayef.Alrefai <159164377+nayef-alrefai@users.noreply.github.com>
2024-10-17 14:07:57 +0000

    working_comments

--- rps.js
+++ rps.js
@@ -1,46 +1,23 @@
-function rockPaperScissors(player1, player2) {
-  const winMatrix = {
-    rock: [`scissors`, `lizard`],
+function rockPaperScissors(player1, player2) { //function takes 2 arguments for the 2 players of the game
+  const winMatrix = { //object acts as a dictionary for indexing the outcome of choosing each item, the keys for each definition are assigned with the objects which lose 
+    rock: [`scissors`, `lizard`], 
     paper: [`rock`, `spock`],
     scissors: [`paper`,`lizard`],
     spock: [`rock`, `scissors`],
     lizard: [`spock`, `paper`],
   };
 
-  if (player1 === player2) {
+  if (player1 === player2) { //if statements act as the logic for the program in this case if both players choose the same item the program responds with "draw"
     return 'draw';
-  } else if (winMatrix[player1].includes(player2)) {
+  } else if (winMatrix[player1].includes(player2)) { //
     return 'player1';
   } else {
     return 'player2';
   }
 }
 
-console.log(rockPaperScissors("spock", "rock")); // should return "player1"
+console.log(rockPaperScissors("spock", "rock"));
 
-//console.log(rockPaperScissors("scissors", "rock"))
-
-/*if (player1 === player2) {
-    return "draw"
-  }
-
-  else if ((player1 === "rock" && player2 === "scissors") || (player1 === "paper" && player2 === "rock") || (player1 === "scissors" && player2 === "paper")) {
-    return "player1"
-    }
-
-  else if ((player1 === "paper" && player2 === "scissors") || (player1 === "scissors" && player2 === "rock") || (player1 === "rock" && player2 === "paper")) {
-    return "player2"
-  }
-  */
-
-
-
-
-
-
-
-
-// Leave this code here for the automated tests
 module.exports = {
   rockPaperScissors,
 }

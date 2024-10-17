1019b7064ca87d155fd35ecc3e7c1d1e2ee545ea
Nayef.Alrefai <159164377+nayef-alrefai@users.noreply.github.com>
2024-10-17 13:58:10 +0000

    working

--- rps.js
+++ rps.js
@@ -1,9 +1,26 @@
-//rock_paper_scissors("paper", "paper")
-
-
 function rockPaperScissors(player1, player2) {
-  
+  const winMatrix = {
+    rock: [`scissors`, `lizard`],
+    paper: [`rock`, `spock`],
+    scissors: [`paper`,`lizard`],
+    spock: [`rock`, `scissors`],
+    lizard: [`spock`, `paper`],
+  };
+
   if (player1 === player2) {
+    return 'draw';
+  } else if (winMatrix[player1].includes(player2)) {
+    return 'player1';
+  } else {
+    return 'player2';
+  }
+}
+
+console.log(rockPaperScissors("spock", "rock")); // should return "player1"
+
+//console.log(rockPaperScissors("scissors", "rock"))
+
+/*if (player1 === player2) {
     return "draw"
   }
 
@@ -14,10 +31,7 @@ function rockPaperScissors(player1, player2) {
   else if ((player1 === "paper" && player2 === "scissors") || (player1 === "scissors" && player2 === "rock") || (player1 === "rock" && player2 === "paper")) {
     return "player2"
   }
-   
-}
-//console.log(rockPaperScissors("scissors", "rock"))
-
+  */
 
 
 

1a394f9f46e3515eb99e13f1e2e4cd27644d9cfe
Nayef.Alrefai <159164377+nayef-alrefai@users.noreply.github.com>
2024-10-22 19:34:37 +0000

    final_final

--- rps.js
+++ rps.js
@@ -1,22 +1,22 @@
-function rockPaperScissors(player1, player2) { //function takes 2 arguments for the 2 players of the game
-  const winMatrix = { //object acts as a dictionary for indexing the outcome of choosing each item, the keys for each definition are assigned with the objects which lose 
-    rock: [`scissors`, `lizard`], 
-    paper: [`rock`, `spock`],
-    scissors: [`paper`,`lizard`],
-    spock: [`rock`, `scissors`],
-    lizard: [`spock`, `paper`],
+function rockPaperScissors(player1, player2) { 
+  // Dictionary of winning choices, the key defeats the other items
+  const winMatrix = { 
+    rock: ['scissors', 'lizard'], 
+    paper: ['rock', 'spock'],
+    scissors: ['paper','lizard'],
+    spock: ['rock', 'scissors'],
+    lizard: ['spock', 'paper'],
   };
 
-  if (player1 === player2) { //if statements act as the logic for the program in this case if both players choose the same item the program responds with "draw"
-    return 'draw';
-  } else if (winMatrix[player1].includes(player2)) { //
-    return 'player1';
-  } else {
-    return 'player2';
-  }
+  //both players choosing the same item results in a draw
+  if (player1 === player2) return 'draw';
+
+  // Checks if player1's item defeats player2's based on the winMatrix
+  // If true, player1 wins otherwise, player2 wins
+  return winMatrix[player1].includes(player2) ? 'player1' : `player2`;
 }
 
-console.log(rockPaperScissors("spock", "rock"));
+console.log(rockPaperScissors('spock', 'rock'));
 
 module.exports = {
   rockPaperScissors,
